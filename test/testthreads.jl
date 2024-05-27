520cd556ceb9175aebec1904dbceb5b1b8acb8d3
Yuning <neuromancer43@gmail.com>
2024-05-27 17:21:28 +0200

    add test to multi-thread

---
+++ test/testthreads.jl
@@ -0,0 +1,28 @@
+using Test
+using SpinShuttling
+using DelimitedFiles
+# using UnicodePlots
+# using ProgressMeter
+
+@testset "test multithreading" begin
+    T=4; L=10; σ = sqrt(2) / 20; N=501; κₜ=1;κₓ=1;
+    
+    B=OrnsteinUhlenbeckField(0,[κₜ,κₓ],σ)
+    model=OneSpinModel(T,L,N,B)
+
+    # save the original Σ to text file
+    if Threads.nthreads() > 1
+        writedlm("test/cache.csv", model.R.Σ)
+    else
+        # read the original Σ from text file
+        Σ = readdlm("test/cache.csv")
+        # test the original Σ is the same as the one in the model
+        @test isapprox(Σ, model.R.Σ, rtol=1e-10)
+        # delete the cache file
+        rm("test/cache.csv", force=true)
+    end
+
+    M = 100000; 
+    F=sampling(model, fidelity, M)
+    println(F)
+end
